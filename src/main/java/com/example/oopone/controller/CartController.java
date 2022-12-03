72afd6d3615b85e148422f72627c4086f736a733
Yraj-m <f20202226@hyderabad.bits-pilani.ac.in>
2022-12-03 18:08:59 +0530

    Changes CartItemsService

--- src/main/java/com/example/oopone/controller/CartController.java
+++ src/main/java/com/example/oopone/controller/CartController.java
@@ -6,30 +6,35 @@ import com.example.oopone.model.CartItems;
 import com.example.oopone.model.User;
 import com.example.oopone.service.CartItemsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 
 @RestController
-
 public class CartController {
 
     @Autowired
     CartItemsService cartItemsService;
 
     @PostMapping("/addToCart/{userid}/{itemid}")
-    public CartItems addToCart(@PathVariable int userid, @PathVariable int itemid){
-        return
-        cartItemsService.saveToCart(userid,itemid);
+    public CartItems addToCart(@PathVariable int userid, @PathVariable int itemid) {
+        return cartItemsService.saveToCart(userid, itemid);
     }
 
     @GetMapping(value = "/cart/user/{id}")
-    public List<CartItems> getCartUser(@PathVariable int id){
+    public List<CartItems> getCartUser(@PathVariable int id) {
         return cartItemsService.getAllCartItems(id);
-   }
+    }
+
+    @DeleteMapping("/removeItem/{userid}/{itemid}")
+    public void removeFromCart(@PathVariable int userid, @PathVariable int itemid) {
+        cartItemsService.removeItemFromCart(userid, itemid);
+    }
+
+   @GetMapping("/cartvalue/{userid}")
+    public double getCartValue(@PathVariable int userid){
+        return cartItemsService.calculateCartValue(userid);
+}
 }
--- src/main/java/com/example/oopone/service/CartItemsService.java
+++ src/main/java/com/example/oopone/service/CartItemsService.java
@@ -41,4 +41,18 @@ public class CartItemsService {
         return cartItemsRepo.findAllByUserid(userid);
     }
 
+    public void removeItemFromCart(int user_id,int item_id){
+        Item item = itemRepo.findById(item_id).get();
+       cartItemsRepo.deleteById(cartItemsRepo.findByUseridAndItem(user_id,item).getId());
+    }
+
+
+    public double calculateCartValue(int user_id){
+        List<CartItems> cartItemsList = cartItemsRepo.findAllByUserid(user_id);
+        double sum = 0;
+        for(CartItems var: cartItemsList){
+            sum+=((var.getQuantity_in_cart())*(var.getItem().getPrice()));
+        }
+        return sum;
+    }
 }
